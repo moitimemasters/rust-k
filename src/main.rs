4159767efd374a7743fd4606bda706b170903a36
onejx <onejx@pop-os.localdomain>
2021-09-28 22:37:53 +0300

    first version

---
+++ src/main.rs
@@ -0,0 +1,217 @@
+use std::{collections::HashMap, env, fs, str::FromStr};
+
+
+use basic_types::KeyWordType;
+use token::Token;
+
+use crate::{basic_types::{OpertaionType, TokenType}, parser::parse_program};
+
+mod token;
+mod basic_types;
+mod parser;
+
+
+pub fn simulate_program(program: &Vec<Token>) {
+    let mut stack: Vec<i64> = Vec::new();
+    let mut id_index_table: HashMap<i64, usize> = HashMap::new();
+    // let mut defer_stack: Vec<i64> = Vec::new();
+    for i in 0..(program.len()) {
+        let token = program[i];
+        match token.get_keyword_type() {
+            None => {}
+            _ => {
+                let id = token.get_id().unwrap();
+                id_index_table.insert(id, i);
+            }
+        }
+    }
+    let mut i: usize = 0;
+    while i < program.len() {
+        let token = program[i];
+        match token {
+            Token { token_type: TokenType::Opertaion, value, defered: _, operation_type, keyword_type: _, comparative_type: _, references: _, id: _ } => {
+                match operation_type {
+                    Some(OpertaionType::Push) => {
+                        if let Some(x) = value {
+                            stack.push(x);
+                        } else {
+                            unreachable!();
+                        }
+                    }
+                    Some(OpertaionType::Dump) => {
+                        let value = stack.pop().unwrap();
+                        println!("{}", value);
+                    }
+                    Some(OpertaionType::Plus) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(b + a);
+                    }
+                    Some(OpertaionType::Minus) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(b - a);
+                    }
+                    Some(OpertaionType::Mul) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(b * a);
+                    }
+                    Some(OpertaionType::Div) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(b / a);
+                    }
+                    Some(OpertaionType::Mod) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(b % a);
+                    }
+                    Some(OpertaionType::Shr) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(b >> a);
+                    }
+                    Some(OpertaionType::Shl) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(b << a);
+                    }
+                    Some(OpertaionType::Band) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(b & a);
+                    }
+                    Some(OpertaionType::Bor) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(b | a);
+                    }
+                    Some(OpertaionType::Xor) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(b ^ a);
+                    }
+                    Some(OpertaionType::Blank) => {
+                        println!();
+                    }
+                    Some(OpertaionType::Dup) => {
+                        let a = stack.pop().unwrap();
+                        stack.push(a);
+                        stack.push(a);
+                    }
+                    Some(OpertaionType::Dup2) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(b);
+                        stack.push(a);
+                        stack.push(b);
+                        stack.push(a);
+                    }
+                    Some(OpertaionType::Swap) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push(a);
+                        stack.push(b);
+                    }
+                    Some(OpertaionType::Swap2) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        let c = stack.pop().unwrap();
+                        let d = stack.pop().unwrap();
+                        stack.push(b);
+                        stack.push(a);
+                        stack.push(d);
+                        stack.push(c);
+                    }
+                    Some(OpertaionType::Pop) => {
+                        stack.pop().unwrap();
+                    }
+                    Some(OpertaionType::Put) => {
+                        let a = stack.pop().unwrap();
+                        print!("{}", a)
+                    }
+                    None => { unreachable!() }
+                }
+            },
+            Token { token_type: TokenType::KeyWord, value, defered, operation_type, keyword_type, comparative_type, references, id} => {
+                match keyword_type {
+                    Some(KeyWordType::While) => {
+
+                    }
+                    Some(KeyWordType::Do) => {
+                        let a = stack.pop().unwrap();
+                        if a == 0 {
+                            let goto = references.unwrap();
+                            i = id_index_table[&goto];
+                        }
+                    }
+                    Some(KeyWordType::End) => {
+                        i = id_index_table[&references.unwrap()];
+                    }
+                    None => { unreachable!() }
+                }
+            },
+            Token { token_type: TokenType::Comparative, value, defered, operation_type, keyword_type, comparative_type, references, id } => {
+                match comparative_type {
+                    Some(basic_types::ComparativeType::Less) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push((b < a) as i64);
+                    }
+                    Some(basic_types::ComparativeType::LessEq) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push((b <= a) as i64);
+                    }
+                    Some(basic_types::ComparativeType::Greater) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push((b > a) as i64);
+                    }
+                    Some(basic_types::ComparativeType::GreaterEq) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push((b >= a) as i64);
+                    }
+                    Some(basic_types::ComparativeType::Equal) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push((b == a) as i64);
+                    }
+                    Some(basic_types::ComparativeType::And) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push((b != 0 && a != 0) as i64);
+                    }
+                    Some(basic_types::ComparativeType::Or) => {
+                        let a = stack.pop().unwrap();
+                        let b = stack.pop().unwrap();
+                        stack.push((b != 0 || a != 0) as i64);
+                    }
+                    Some(basic_types::ComparativeType::Not) => {
+                        let a = stack.pop().unwrap();
+                        stack.push((a == 0) as i64);
+                    }
+                    None => { unreachable!() }
+                }
+            }
+            _ => { unreachable!() }
+        }
+        i += 1;
+    }
+}
+
+fn main() {
+    // let program: Vec<Token> = vec![Token::op_push(34), Token::op_push(35), Token::op_mul(), Token::op_dump()];
+    // simulate_program(&program);
+    // println!("\n\n\nProgram Started:");
+    // let a: String = String::from_str("1 while dup 5 <= do dup dump 1 + end").unwrap();
+    // let program = parse_program(a);
+    // simulate_program(&program);
+    let input_konch_file = env::args().nth(1).expect("no input file");
+    let string_input = fs::read_to_string(input_konch_file).expect("failed opening file");
+    // println!("{}", string_input);
+    let program = parse_program(string_input);
+    simulate_program(&program);
+}
---
+++ src/token.rs
@@ -0,0 +1,254 @@
+use crate::basic_types::{ComparativeType, KeyWordType, OpertaionType, TokenType};
+
+#[derive(Clone, Copy)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub value: Option<i64>,
+    pub defered: bool,
+    pub operation_type: Option<OpertaionType>,
+    pub keyword_type: Option<KeyWordType>,
+    pub comparative_type: Option<ComparativeType>,
+    pub references: Option<i64>,
+    pub id: Option<i64>,
+}
+
+impl Token {
+    pub fn new(
+        token_type: TokenType,
+        operation_type: Option<OpertaionType>,
+        keyword_type: Option<KeyWordType>,
+        comparative_type: Option<ComparativeType>,
+        value: Option<i64>,
+        defered: bool,
+        id: Option<i64>,
+        references: Option<i64>,
+    ) -> Token {
+        Token {
+            token_type,
+            operation_type,
+            keyword_type,
+            comparative_type,
+            value,
+            defered,
+            references,
+            id,
+        }
+    }
+
+    pub fn change_defered(&mut self, defer: bool) {
+        self.defered = defer;
+    }
+
+    pub fn change_reference(&mut self, references: Option<i64>) {
+        self.references = references;
+    }
+
+    pub fn get_keyword_type(self) -> Option<KeyWordType> {
+        self.keyword_type
+    }
+
+    pub fn get_comparative_type(self) -> Option<ComparativeType> {
+        self.comparative_type
+    }
+
+    pub fn get_operation_type(self) -> Option<OpertaionType> {
+        self.operation_type
+    }
+
+    pub fn get_id(self) -> Option<i64> {
+        self.id
+    }
+
+    fn op_no_args_operation(operation_type: OpertaionType) -> Token {
+        Token::new(
+            TokenType::Opertaion,
+            Some(operation_type),
+            None,
+            None,
+            None,
+            false,
+            None,
+            None,
+        )
+    }
+    fn cmp_no_args_operation(cmp_type: ComparativeType) -> Token {
+        Token::new(
+            TokenType::Comparative,
+            None,
+            None,
+            Some(cmp_type),     
+            None,
+            false,
+            None,
+            None,
+        )
+    }
+}
+
+// opertaions with args
+impl Token {
+    pub fn op_push(x: i64) -> Token {
+        Token::new(
+            TokenType::Opertaion,
+            Some(OpertaionType::Push),
+            None,
+            None,
+            Some(x),
+            false,
+            None,
+            None,
+        )
+    }
+}
+
+// keywords
+impl Token {
+    pub fn kw_while(id: i64) -> Token {
+        Token::new(
+            TokenType::KeyWord,
+            None,
+            Some(KeyWordType::While),
+            None,
+            None,
+            false,
+            Some(id),
+            Some(id),
+        )
+    }
+
+    pub fn kw_end(id: i64, references: i64) -> Token {
+        Token::new(
+            TokenType::KeyWord,
+            None,
+            Some(KeyWordType::End),
+            None,
+            None,
+            false,
+            Some(references),
+            Some(id),
+        )
+    }
+
+    pub fn kw_do(id: i64, references: i64) -> Token {
+        Token::new(
+            TokenType::KeyWord,
+            None,
+            Some(KeyWordType::Do),
+            None,
+            None,
+            false,
+            Some(references),
+            Some(id),
+        )
+    }
+}
+
+
+// no args operations
+impl Token {
+    pub fn op_dump() -> Token {
+        Token::op_no_args_operation(OpertaionType::Dump)
+    }
+
+    pub fn op_plus() -> Token {
+        Token::op_no_args_operation(OpertaionType::Plus)
+    }
+
+    pub fn op_minus() -> Token {
+        Token::op_no_args_operation(OpertaionType::Minus)
+    }
+
+    pub fn op_mul() -> Token {
+        Token::op_no_args_operation(OpertaionType::Mul)
+    }
+
+    pub fn op_div() -> Token {
+        Token::op_no_args_operation(OpertaionType::Div)
+    }
+
+    pub fn op_mod() -> Token {
+        Token::op_no_args_operation(OpertaionType::Mod)
+    }
+
+    pub fn op_shr() -> Token {
+        Token::op_no_args_operation(OpertaionType::Shr)
+    }
+
+    pub fn op_shl() -> Token {
+        Token::op_no_args_operation(OpertaionType::Shl)
+    }
+
+    pub fn op_band() -> Token {
+        Token::op_no_args_operation(OpertaionType::Band)
+    }
+
+    pub fn op_bor() -> Token {
+        Token::op_no_args_operation(OpertaionType::Bor)
+    }
+
+    pub fn op_xor() -> Token {
+        Token::op_no_args_operation(OpertaionType::Xor)
+    }
+
+    pub fn op_blank() -> Token {
+        Token::op_no_args_operation(OpertaionType::Blank)
+    }
+
+
+    pub fn op_dup() -> Token {
+        Token::op_no_args_operation(OpertaionType::Dup)
+    }
+
+    pub fn op_dup2() -> Token {
+        Token::op_no_args_operation(OpertaionType::Dup2)
+    }
+
+    pub fn op_swap() -> Token {
+        Token::op_no_args_operation(OpertaionType::Swap)
+    }
+
+    pub fn op_pop() -> Token {
+        Token::op_no_args_operation(OpertaionType::Pop)
+    }
+
+    pub fn op_put() -> Token {
+        Token::op_no_args_operation(OpertaionType::Put)
+    }
+
+    pub fn op_swap2() -> Token {
+        Token::op_no_args_operation(OpertaionType::Swap2)
+    }
+
+    pub fn cmp_less() -> Token {
+        Token::cmp_no_args_operation(ComparativeType::Less)
+    }
+
+    pub fn cmp_less_eq() -> Token {
+        Token::cmp_no_args_operation(ComparativeType::LessEq)
+    }
+
+    pub fn cmp_greater() -> Token {
+        Token::cmp_no_args_operation(ComparativeType::Greater)
+    }
+
+    pub fn cmp_greater_eq() -> Token {
+        Token::cmp_no_args_operation(ComparativeType::GreaterEq)
+    }
+
+    pub fn cmp_equal() -> Token {
+        Token::cmp_no_args_operation(ComparativeType::Equal)
+    }
+
+    pub fn cmp_and() -> Token {
+        Token::cmp_no_args_operation(ComparativeType::And)
+    }
+
+    pub fn cmp_or() -> Token {
+        Token::cmp_no_args_operation(ComparativeType::Or)
+    }
+
+    pub fn cmp_not() -> Token {
+        Token::cmp_no_args_operation(ComparativeType::Not)
+    }
+
+}
